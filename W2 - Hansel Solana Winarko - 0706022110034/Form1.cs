873a0b21e24750d69b296d56a2a48ec234d3bf00
Hansel Solana Winarko <hanselsolanawinarko@gmail.com>
2022-03-14 22:37:24 +0700

    Add project files.

---
+++ W2 - Hansel Solana Winarko - 0706022110034/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W2___Hansel_Solana_Winarko___0706022110034
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TextBoxInputData.Text.Substring(0, 4) == "ISI:")
+            {
+                LabelHasil.Text = TextBoxInputData.Text.Substring(4);
+            }
+        }
+    }
+}
